b37a052208aea0d65cabf8744a46e5df8b747021
Jose Clemente Garcia Rodriguez <josegarciarodriguez89@hotmail.com>
2023-02-19 16:47:57 +0100

    Usuario service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     SharedModule,
     UsuariosModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/usuarios/lista/lista.component.ts
+++ src/app/usuarios/lista/lista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-lista',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UsuarioService) { }
 
   ngOnInit(): void {
+    this.userService.getUser().subscribe(users => {
+      console.log("users", users);
+    });
   }
 
 }
